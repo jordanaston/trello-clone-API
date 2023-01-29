3ceb3e076a77f08dca89db8ee9a1d4350c83060a
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-01-29 22:43:58 +1000

    controllers, schemas and commands files built

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app():
     # using a list comprehension and multiple assignment 
@@ -12,8 +18,21 @@ def create_app():
 
     # configuring our app:
     app.config.from_object("config.app_config")
-
+    
     # creating our database object! This allows us to use our ORM
-    db = SQLAlchemy(app)
+    db.init_app(app)
+
+    # creating our marshmallow object! This allows us to use schemas
+    ma.init_app(app)
+
+    #creating the jwt and bcrypt objects! this allows us to use authentication
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    # import the controllers and activate the blueprints
+    from controllers import registerable_controllers
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
     
     return app
